961d31f264b6c748d6c2575118567909f66feef2
nada salloum <117869895+nadasalloum@users.noreply.github.com>
2023-10-23 18:04:44 +0300

    added ultrasonic files

---
+++ stm32_rc_car/Core/Src/Ultrasonic_Task.h
@@ -0,0 +1,14 @@
+#ifndef ULTRASONIC_TASK_H_
+#define ULTRASONIC_TASK_H_
+
+#include "ultrasonic_interface.h"
+
+typedef enum{
+	Green_Flag,
+	Red_Flag
+}ULTRASONIC_PASS;
+
+void Ultrasonic_Task (void*pvParameter );
+void Ultrasonictest_Task(void*pvParameter);
+
+#endif //ULTRASONIC_TASK_H_
