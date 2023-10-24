a6a9c417d0517ad3e687aa4b8bc401324632aa6c
SalmaFaragalla <142256837+SalmaFaragalla@users.noreply.github.com>
2023-10-24 23:58:04 +0300

    update: comments

--- stm32_rc_car/Core/DCM/DCM_config.h
+++ stm32_rc_car/Core/DCM/DCM_config.h
@@ -5,18 +5,21 @@
  *      Author: Salma
  */
 
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef DCM_DCM_CONFIG_H_
 #define DCM_DCM_CONFIG_H_
 
+/* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_hal.h"
 
-/* Left Motors Configurations */
+/* Exported constants --------------------------------------------------------*/
+/* Left motor configurations */
 #define DCM_LEFT_PORT	(GPIOA)
 #define DCM_LEFT_IN1	(GPIO_PIN_4)
 #define DCM_LEFT_IN2	(GPIO_PIN_5)
 #define DCM_LEFT_EN     (GPIO_PIN_7)
 
-/* Right Motors Configurations */
+/* Right motor configurations */
 #define DCM_RIGHT_PORT	(GPIOA)
 #define DCM_RIGHT_IN1	(GPIO_PIN_2)
 #define DCM_RIGHT_IN2	(GPIO_PIN_3)
