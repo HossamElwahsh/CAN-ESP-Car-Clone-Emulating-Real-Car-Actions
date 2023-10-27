8528e09ea12be6159325be80ff6efdf2eae1282e
nada salloum <117869895+nadasalloum@users.noreply.github.com>
2023-10-27 13:16:07 +0200

    Update: to be continued

--- stm32_rc_car/Core/Src/ultrasonic.c
+++ stm32_rc_car/Core/Src/ultrasonic.c
@@ -4,10 +4,11 @@
 #include "stm32f1xx_hal.h"
 extern TIM_HandleTypeDef htim1;
 extern void delay_us(uint16_t time );
-static uint32_t count;
-static uint16_t TIM1_ULTRA;
-static uint16_t TIM2_ULTRA;
+static Flag_enteringFun=0;
+ static uint16_t TIM1_ULTRA;
+ static uint16_t TIM2_ULTRA;
 static uint8_t ULTRASONIC_FLAG;
+uint16_t distance;
 extern transmission_en  gl_transmission_en;
 
 void delay_us (uint16_t us)
@@ -17,84 +18,106 @@ void delay_us (uint16_t us)
 }
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
+    if(ULTRASONIC_FLAG==0)
+    {
+        if(gl_transmission_en ==Drive)
+        {
+            TIM1_ULTRA= HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_1 );
+            __HAL_TIM_SET_CAPTUREPOLARITY(htim,TIM_CHANNEL_1,TIM_INPUTCHANNELPOLARITY_FALLING);
+        }
+        else if(gl_transmission_en ==Reverse)
+        {
+            TIM1_ULTRA= HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_4 );
+            __HAL_TIM_SET_CAPTUREPOLARITY(htim,TIM_CHANNEL_4,TIM_INPUTCHANNELPOLARITY_FALLING);
+        }
+        ULTRASONIC_FLAG=1;
+    }
+    else if(ULTRASONIC_FLAG==1)
+    {
+        if(gl_transmission_en ==Drive)
+        {
+            TIM2_ULTRA= HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_1 );
+            __HAL_TIM_SET_COUNTER(htim,0);
+            __HAL_TIM_SET_CAPTUREPOLARITY(htim,TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
+            __HAL_TIM_DISABLE_IT(&htim1,TIM_IT_CC1);
+        }
+        else if(gl_transmission_en ==Reverse)
+        {
 
-	if(ULTRASONIC_FLAG==0){
-	if(gl_transmission_en ==Drive){
-	TIM1_ULTRA= HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_1 );
-	__HAL_TIM_SET_CAPTUREPOLARITY(htim,TIM_CHANNEL_1 ,TIM_INPUTCHANNELPOLARITY_FALLING);
-	}
-	else if(gl_transmission_en ==Reverse){
-		TIM1_ULTRA= HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_4 );
-		__HAL_TIM_SET_CAPTUREPOLARITY(htim,TIM_CHANNEL_4 ,TIM_INPUTCHANNELPOLARITY_FALLING);
-	}
-	ULTRASONIC_FLAG=1;
+            TIM2_ULTRA= HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_4 );
 
-	}
-	else if(ULTRASONIC_FLAG==1){
-     if(gl_transmission_en ==Drive){
-	TIM2_ULTRA= HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_1 );
+            __HAL_TIM_SET_COUNTER(htim,0);
+            __HAL_TIM_SET_CAPTUREPOLARITY(htim,TIM_CHANNEL_4, TIM_INPUTCHANNELPOLARITY_RISING);
+            __HAL_TIM_DISABLE_IT(&htim1,TIM_IT_CC4);
+        }
 
-	__HAL_TIM_SET_COUNTER(htim,0);
-	__HAL_TIM_SET_CAPTUREPOLARITY(htim,TIM_CHANNEL_1 , TIM_INPUTCHANNELPOLARITY_RISING);
-	__HAL_TIM_DISABLE_IT(&htim1,TIM_IT_CC1);
-     }
-     else if(gl_transmission_en ==Reverse){
 
-    	 TIM2_ULTRA= HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_4 );
+        ULTRASONIC_FLAG=2;
+    }
 
-    	 	__HAL_TIM_SET_COUNTER(htim,0);
-    	 	__HAL_TIM_SET_CAPTUREPOLARITY(htim,TIM_CHANNEL_4 , TIM_INPUTCHANNELPOLARITY_RISING);
-    	 	__HAL_TIM_DISABLE_IT(&htim1,TIM_IT_CC4);
-     }
+    if(ULTRASONIC_FLAG==2)
+    {
+        if(TIM2_ULTRA>TIM1_ULTRA)
+        {
+            DIFFERNCE=TIM2_ULTRA-TIM1_ULTRA;
+        }
+        else
+        {
+            DIFFERNCE=(0xffff-TIM1_ULTRA)+TIM2_ULTRA;
+        }
+    distance=DIFFERNCE* 0.034/2;
+    ULTRASONIC_FLAG=0;   
+    }
+
+
+    else
+    {
+        //donthing
+
+    }
+ 
+
+
 
 	
-		ULTRASONIC_FLAG=2;
-
-	}
 
 }
 
-ULTRASONIC_READ Ultrasonic_getdistance(transmission_en  transmission_Ultra,uint16_t* DISTANCE){
+void Ultrasonic_getdistance(transmission_en  transmission_Ultra){
 	uint16_t DIFFERNCE;
-	ULTRASONIC_READ STATUS;
+	static Flag_enteringFun=0;
+	 Flag_enteringFun++;
+	if(Flag_enteringFun>1&&ULTRASONIC_FLAG==1){
+          ULTRASONIC_FLAG=0;	
+	}
+	else{
+	Flag_enteringFun=0;	
+	}
 switch	(transmission_Ultra){
 case Drive:
 	HAL_GPIO_WritePin(TRIGER_FORWARD_PORT ,TRIGER_FORWARD_PIN , GPIO_PIN_SET);
 	    delay_us(10);
 	HAL_GPIO_WritePin(TRIGER_FORWARD_PORT , TRIGER_FORWARD_PIN , GPIO_PIN_RESET);
 	__HAL_TIM_ENABLE_IT(&htim1,TIM_IT_CC1);
-	while(ULTRASONIC_FLAG<2&&count<time_out ){
-	count++;
-       }
 	break;
   case Reverse:
 	HAL_GPIO_WritePin(TRIGER_BACKWARD_PORT , TRIGER_BACKWARD_PIN, GPIO_PIN_SET);
 	    delay_us(10);
 	HAL_GPIO_WritePin(TRIGER_BACKWARD_PORT ,TRIGER_BACKWARD_PIN, GPIO_PIN_RESET);
 	__HAL_TIM_ENABLE_IT(&htim1,TIM_IT_CC4);
-	while(ULTRASONIC_FLAG<2&&count<time_out ){
-	      count++;
-           }
+
 	break;
     default:
 	break;
+      }
+
 }
-	    if(ULTRASONIC_FLAG==2){
-		if(TIM2_ULTRA>TIM1_ULTRA){
-		DIFFERNCE=TIM2_ULTRA-TIM1_ULTRA;
-		}
-	   else{
-		DIFFERNCE=(0xffff-TIM1_ULTRA)+TIM2_ULTRA;
-		}
-		*DISTANCE=DIFFERNCE* 0.034/2;
-		ULTRASONIC_FLAG=0;
-		count=0;
-		STATUS=SUCCESS_;
-		}
-		else{
-		STATUS=TIME_OUT_;
-		count=0;
-		}
-		
-return STATUS;
+uint16_t  Ultrasonc_getdistace(void){
+
+return  distance;
+
 }
+
+
+
+
