947626f3a43dff01f9a46eb04881b1e1966fe2b5
nada salloum <117869895+nadasalloum@users.noreply.github.com>
2023-10-29 01:44:29 +0200

    Update Ultrasonic_Task.h Nada

--- stm32_rc_car/Core/Inc/Ultrasonic_Task.h
+++ stm32_rc_car/Core/Inc/Ultrasonic_Task.h
@@ -2,14 +2,18 @@
 #define ULTRASONIC_TASK_H_
 
 #include "ultrasonic_interface.h"
-
 typedef enum{
 	Green_Flag,
 	Red_Flag
 }ULTRASONIC_PASS;
 
+typedef enum{
+	Success_=8,
+	Time_Out_=11
+}ULTRASONIC_Status;
+
 void Ultrasonic_Task (void*pvParameter );
+void Ultrasonic_Timeout_Task(void*pvParameter);
 void Ultrasonictest_Task(void*pvParameter);
-void delay_us (uint16_t us);
 
 #endif //ULTRASONIC_TASK_H_
